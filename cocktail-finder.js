dcdb9de19664dcdbe516cbc3b8b7f6dd87c0d6aa
Gnarlyluck <schuylerluckey@gmail.com>
2020-10-20 08:33:52 -0700

    got ingredients, instructions, image, name  and glass type tp print to the window

--- cocktail-finder.js
+++ cocktail-finder.js
@@ -1,36 +1,121 @@
 const BASE_URL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=`
 
-
-const log = document.getElementById('cocktail-results')
+let drinkLog = document.getElementById('cocktail-results')
 function updateValue(e){
-    log.textContent = e.target.value
+    drinkLog/*.textContent*/ = e.target.value
 }
-//receive user cocktail text in input bar!!!!
-//take user input and insert it onto the end of API URL using string interpolation!!!
-//once inserted initialize or 'fire' the click button to return results!!!
-//user should also be able to hit enter for results
-//after accessing your selected drink go into the 
-//drink name
-//instructions
-//glass type
-//ingredient 1, 2, 3, 4 etc
-//measure e, 2, 3, 4, etc
-//if there is no value stop running
-//results should be appended to a <p> tag or <div> etc .
-const getCocktail = async () => {
-   let userInput = document.getElementById("cocktailInput").value;
-                 
-   const COCKTAIL_URL = `${BASE_URL}${userInput}`
+const getCocktail = (event) => {
+    event.preventDefault()
+   let userInput = document.getElementById("cocktailInput").value;             
+   
+    displayCocktail(userInput)
+}
+
+let cocktailIngredients = document.querySelector('.ingredients')
+
+const displayCocktail = async (userInput) => {
     try{
-    const response = await axios.get(COCKTAIL_URL)
-    console.log(response)
+        const response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${userInput}`)
+        let drink = response.data.drinks[0]
+        console.log(drink)
+        let drinkName = drink.strDrink
+        console.log(drinkName)
+        let glass = drink.strGlass
+        console.log(glass)
+        let image = drink.strDrinkThumb
+        console.log(image)
+        let instructions = drink.strInstructions
+        console.log(instructions)
+        let showDrink = document.querySelector('#showDrink')
+        showDrink.innerHTML = 
+        `<div class = 'showCocktail'> 
+        <p id='cocktailName'> ${drinkName}</p>
+        <img src=${image} id='cocktailImage'/>`
+        cocktailIngredients.innerHTML=`
+        <h3> Ingredients </h3>
+        <span id='measurement1'>${drink.strMeasure1 }</span>
+        <span id='ingredient1'>${drink.strIngredient1 }</span>`
+        if(drink.strIngredient2 != null){
+            cocktailIngredients.innerHTML +=
+            `<span id='measurement2'>${drink.strMeasure2 }</span>
+        <span id='ingredient2'>${drink.strIngredient2 }</span>`
+        }
+        if(drink.strIngredient3 != null){
+            cocktailIngredients.innerHTML +=
+            `<span id='measurement3'>${drink.strMeasure3 }</span>
+        <span id='ingredient3'>${drink.strIngredient3 }</span>`
+        }
+        if(drink.strIngredient4 != null){
+            cocktailIngredients.innerHTML +=
+            `<span id='measurement4'>${drink.strMeasure4 }</span>
+        <span id='ingredient4'>${drink.strIngredient4 }</span>`
+        }
+        if(drink.strIngredient5 != null){
+            cocktailIngredients.innerHTML +=
+            `<span id='measurement5'>${drink.strMeasure5 }</span>
+        <span id='ingredient5'>${drink.strIngredient5 }</span>`
+        }
+        if(drink.strIngredient6 != null){
+            cocktailIngredients.innerHTML +=
+            `<span id='measurement6'>${drink.strMeasure6 }</span>
+        <span id='ingredient6'>${drink.strIngredient6 }</span>`
+        }
+        if(drink.strIngredient7 != null){
+            cocktailIngredients.innerHTML +=
+            `<span id='measurement7'>${drink.strMeasure7 }</span>
+        <span id='ingredient7'>${drink.strIngredient7 }</span>`
+        }
+        if(drink.strIngredient7 != null){
+            cocktailIngredients.innerHTML +=
+            `<span id='measurement7'>${drink.strMeasure7 }</span>
+        <span id='ingredient7'>${drink.strIngredient7 }</span>`
+        }
+        if(drink.strIngredient8 != null){
+            cocktailIngredients.innerHTML +=
+            `<span id='measurement8'>${drink.strMeasure8 }</span>
+        <span id='ingredient8'>${drink.strIngredient8 }</span>`
+        }
+        if(drink.strIngredient8 != null){
+            cocktailIngredients.innerHTML +=
+            `<span id='measurement8'>${drink.strMeasure9 }</span>
+        <span id='ingredient8'>${drink.strIngredient9 }</span>`
+        }
+        if(drink.strIngredient9 != null){
+            cocktailIngredients.innerHTML +=
+            `<span id='measurement9'>${drink.strMeasure10 }</span>
+        <span id='ingredient9'>${drink.strIngredient10 }</span>`
+        }
+        if(drink.strIngredient9 != null){
+            cocktailIngredients.innerHTML +=
+            `<span id='measurement9'>${drink.strMeasure11 }</span>
+        <span id='ingredient9'>${drink.strIngredient11 }</span>`
+        }
+        if(drink.strIngredient10 != null){
+            cocktailIngredients.innerHTML +=
+            `<span id='measurement10'>${drink.strMeasure12 }</span>
+        <span id='ingredient10'>${drink.strIngredient12 }</span>`
+        }
+        if(drink.strIngredient11 != null){
+            cocktailIngredients.innerHTML +=
+            `<span id='measurement11'>${drink.strMeasure13 }</span>
+        <span id='ingredient11'>${drink.strIngredient13 }</span>`
+        }
+        if(drink.strIngredient12 != null){
+            cocktailIngredients.innerHTML +=
+            `<span id='measurement12'>${drink.strMeasure14 }</span>
+        <span id='ingredient12'>${drink.strIngredient14 }</span>`
+        }
+        if(drink.strIngredient13 != null){
+            cocktailIngredients.innerHTML +=
+            `<span id='measurement13'>${drink.strMeasure15 }</span>
+        <span id='ingredient13'>${drink.strIngredient15 }</span>`
+        }  
+        showDrink.innerHTML += `<h4>glass </h4><p id='glassType'>${glass}</p>`
+        showDrink.innerHTML += `<h4>Instructions </h4><p id='preparation'>${instructions}</p>`
     }catch(error){
-    console.log(error)
+        console.log(error)
     }
+
 }
-
-
-
-
 document.querySelector('input').addEventListener('input', updateValue)
 document.querySelector('.cocktailbtn').addEventListener('click', getCocktail)
