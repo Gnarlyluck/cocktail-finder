2617bd8bd412741d20dd7b298ec6dfe910d381e8
Gnarlyluck <schuylerluckey@gmail.com>
2020-10-22 14:46:07 -0700

    feel about done with the css, edited code and deleted uneccesary lines and console.log

--- cocktail-finder.js
+++ cocktail-finder.js
@@ -5,123 +5,76 @@ let cocktailIngredients = document.querySelector('.ingredients')
 let drinkLog = document.getElementById('cocktail-results')
 let showDrink = document.querySelector('#showDrink')
 
-
-
 function updateValue(e){
     drinkLog = e.target.value
 }
 const getCocktail = (event) => {
     event.preventDefault()
-   let userInput = document.getElementById("cocktailInput").value             
-   
+    let userInput = document.getElementById("cocktailInput").value             
     displayCocktail(userInput)
 }
 
-
 const randomCocktail = async () => {
     try{    const response = await axios.get(`${BASE_URL}${random}`)
-    
         let drink = response.data.drinks[0]
         let drinkName = drink.strDrink
         let glass = drink.strGlass
         let image = drink.strDrinkThumb
         let instructions = drink.strInstructions
-        let ingredient = [
-            drink.strIngredient1,
-            drink.strIngredient2,
-            drink.strIngredient3,
-            drink.strIngredient4,
-            drink.strIngredient5,
-            drink.strIngredient6,
-            drink.strIngredient7,
-            drink.strIngredient8,
-            drink.strIngredient9,
-            drink.strIngredient10,
-            drink.strIngredient11,
-            drink.strIngredient12,
-            drink.strIngredient13,
-        ]
-        for(let i = 0; i < ingredient.length; i++){
-            // if(ingredient[i] !== null){
-            // }
-            let measure = [
-                drink.strMeasure1,
-                drink.strMeasure2,
-                drink.strMeasure3,
-                drink.strMeasure4,
-                drink.strMeasure5,
-                drink.strMeasure6,
-                drink.strMeasure7,
-                drink.strMeasure8,
-                drink.strMeasure9,
-                drink.strMeasure10,
-                drink.strMeasure11,
-                drink.strMeasure12,
-                drink.strMeasure13,
-                drink.strMeasure14,
-            ]
-            for(let i = 0; i < measure.length; i++){
-                // if(measure[i] !== null){
-                // }
-            }
             showDrink.innerHTML = 
-            `<div class = 'showCocktail'> 
-            <p id='cocktailName'> ${drinkName}</p>
-            <img src=${image} id='cocktailImage'/>`
+                `<div class = 'showCocktail'> 
+                <p id='cocktailName'> ${drinkName}</p>
+                <img src=${image} id='cocktailImage'/>`
             cocktailIngredients.innerHTML=
-            `<h3 class='subject'> Ingredients </h3>
-            <span class='measurement'>${drink.strMeasure1 } of </span>
-            <span class='ingredient'>${drink.strIngredient1 },  </span>`
+                `<h3 class='subject'> Ingredients </h3>
+                <span class='measurement'>${drink.strMeasure1 } of </span>
+                <span class='ingredient'>${drink.strIngredient1 },  </span>`
             if(drink.strIngredient2 != null){
                 cocktailIngredients.innerHTML +=
                 `<span class='measurement'>${ drink.strMeasure2 } of </span>
-            <span class='ingredient'>${ drink.strIngredient2 }, </span>`
+                <span class='ingredient'>${ drink.strIngredient2 }, </span>`
             }if(drink.strIngredient3 != null){
                 cocktailIngredients.innerHTML +=
                 `<span class='measurement'>${drink.strMeasure3 } of </span>
-            <span class='ingredient'>${drink.strIngredient3 }, </span>`
+                <span class='ingredient'>${drink.strIngredient3 }, </span>`
             }
             if(drink.strIngredient4 != null){
                 cocktailIngredients.innerHTML +=
                 `<span class='measurement'>${drink.strMeasure4 } of </span>
-            <span class='ingredient'>${drink.strIngredient4 }, </span>`
+                <span class='ingredient'>${drink.strIngredient4 }, </span>`
             }
             if(drink.strIngredient5 != null){
                 cocktailIngredients.innerHTML +=
                 `<span class='measurement'>${drink.strMeasure5 } of </span>
-            <span class='ingredient'>${drink.strIngredient5 }, </span>`
+                <span class='ingredient'>${drink.strIngredient5 }, </span>`
             }
             if(drink.strIngredient6 != null){
                 cocktailIngredients.innerHTML +=
                 `<span class='measurement'>${drink.strMeasure6 } of </span>
-            <span class='ingredient'>${drink.strIngredient6 }, </span>`
+                <span class='ingredient'>${drink.strIngredient6 }, </span>`
             }
             if(drink.strIngredient7 != null){
                 cocktailIngredients.innerHTML +=
                 `<span class='measurement'>${drink.strMeasure7 } of </span>
-            <span class='ingredient'>${drink.strIngredient7 }, </span>`
+                <span class='ingredient'>${drink.strIngredient7 }, </span>`
             }
             if(drink.strIngredient8 != null){
                 cocktailIngredients.innerHTML +=
                 `<span class'measurement'>${drink.strMeasure8 } of </span>
-            <span class'ingredient'>${drink.strIngredient8 }, </span>`
+                <span class'ingredient'>${drink.strIngredient8 }, </span>`
             }
             if(drink.strIngredient9 != null){
                 cocktailIngredients.innerHTML +=
                 `<span class'measurement'>${drink.strMeasure9 } of </span>
-            <span class'ingredient'>${drink.strIngredient9 }, </span>`
+                <span class'ingredient'>${drink.strIngredient9 }, </span>`
             }
             if(drink.strIngredient10 != null){
                 cocktailIngredients.innerHTML +=
                 `<span class='measurement'>${drink.strMeasure10 } of </span>
-            <span class='ingredient'>${drink.strIngredient10 }, </span>`
+                <span class='ingredient'>${drink.strIngredient10 }, </span>`
             }
-
-
             showDrink.innerHTML += `<h4 class='subject'>Glass </h4><p id='glassType'>${glass}</p>`
             showDrink.innerHTML += `<h4 class='subject'>Instructions </h4><p id='preparation'>${instructions}</p>`
-        }
-        
         }catch(error){
             alert('that drink is not in our database')
             console.log('random button not wokring')
@@ -132,107 +85,70 @@ const displayCocktail = async (userInput) => {
     try{
         const response = await axios.get(`${BASE_URL}${search}${userInput}`)
         let drink = response.data.drinks[0]
-        console.log(drink)
         let drinkName = drink.strDrink
-        console.log(drinkName)
         let glass = drink.strGlass
-        console.log(glass)
         let image = drink.strDrinkThumb
-        console.log(image)
         let instructions = drink.strInstructions
-        console.log(instructions)
-        
         showDrink.innerHTML = 
-        `<div class = 'showCocktail'> 
-        <p id='cocktailName'> ${drinkName}</p>
-        <img src=${image} id='cocktailImage'/>`
+            `<div class = 'showCocktail'> 
+            <p id='cocktailName'> ${drinkName}</p>
+            <img src=${image} id='cocktailImage'/>`
         cocktailIngredients.innerHTML=`
-        <h3 class='subject'> Ingredients </h3>
-        <span class='measurement'>${drink.strMeasure1 } of </span>
-        <span class='ingredient'>${drink.strIngredient1 },  </span>`
+            <h3 class='subject'> Ingredients </h3>
+            <span class='measurement'>${drink.strMeasure1 } of </span>
+            <span class='ingredient'>${drink.strIngredient1 },  </span>`
         if(drink.strIngredient2 != null){
             cocktailIngredients.innerHTML +=
             `<span class='measurement'>${ drink.strMeasure2 } of </span>
-        <span class='ingredient'>${ drink.strIngredient2 }, </span>`
+            <span class='ingredient'>${ drink.strIngredient2 }, </span>`
         }
         if(drink.strIngredient3 != null){
             cocktailIngredients.innerHTML +=
             `<span class='measurement'>${drink.strMeasure3 } of </span>
-        <span class='ingredient'>${drink.strIngredient3 }, </span>`
+            <span class='ingredient'>${drink.strIngredient3 }, </span>`
         }
         if(drink.strIngredient4 != null){
             cocktailIngredients.innerHTML +=
             `<span class='measurement'>${drink.strMeasure4 } of </span>
-        <span class='ingredient'>${drink.strIngredient4 }, </span>`
+            <span class='ingredient'>${drink.strIngredient4 }, </span>`
         }
         if(drink.strIngredient5 != null){
             cocktailIngredients.innerHTML +=
             `<span class='measurement'>${drink.strMeasure5 } of </span>
-        <span class='ingredient'>${drink.strIngredient5 }, </span>`
+             <span class='ingredient'>${drink.strIngredient5 }, </span>`
         }
         if(drink.strIngredient6 != null){
             cocktailIngredients.innerHTML +=
             `<span class='measurement'>${drink.strMeasure6 } of </span>
-        <span class='ingredient'>${drink.strIngredient6 }, </span>`
+            <span class='ingredient'>${drink.strIngredient6 }, </span>`
         }
         if(drink.strIngredient7 != null){
             cocktailIngredients.innerHTML +=
             `<span class='measurement'>${drink.strMeasure7 } of </span>
-        <span class='ingredient'>${drink.strIngredient7 }, </span>`
+            <span class='ingredient'>${drink.strIngredient7 }, </span>`
         }
         if(drink.strIngredient8 != null){
             cocktailIngredients.innerHTML +=
             `<span class='measurement'>${drink.strMeasure8 } of </span>
-        <span class='ingredient'>${drink.strIngredient8 }, </span>`
+            <span class='ingredient'>${drink.strIngredient8 }, </span>`
         }
         if(drink.strIngredient9 != null){
             cocktailIngredients.innerHTML +=
             `<span class='measurement'>${drink.strMeasure9 } of </span>
-        <span class='ingredient'>${drink.strIngredient9 }, </span>`
+            <span class='ingredient'>${drink.strIngredient9 }, </span>`
         }
         if(drink.strIngredient10 != null){
             cocktailIngredients.innerHTML +=
             `<span class='measurement'>${drink.strMeasure10 } of </span>
-        <span class='ingredient'>${drink.strIngredient10 }, </span>`
+            <span class='ingredient'>${drink.strIngredient10 }, </span>`
         }
-        if(drink.strIngredient11 != null){
-            cocktailIngredients.innerHTML +=
-            `<span class='measurement'>${drink.strMeasure11 } of </span>
-        <span class='ingredient'>${drink.strIngredient11 }, </span>`
-        }
-        if(drink.strIngredient10 != null){
-            cocktailIngredients.innerHTML +=
-            `<span class='measurement'>${drink.strMeasure12 } of </span>
-        <span class='ingredient'>${drink.strIngredient12 }, </span>`
-        }
-        if(drink.strIngredient11 != null){
-            cocktailIngredients.innerHTML +=
-            `<span class='measurement'>${drink.strMeasure13 } of </span>
-        <span class='ingredient'>${drink.strIngredient13 }, </span>`
-        }
-        if(drink.strIngredient12 != null){
-            cocktailIngredients.innerHTML +=
-            `<span class='measurement'>${drink.strMeasure14 } of </span>
-        <span class='ingredient'>${drink.strIngredient14 }, </span>`
-        }
-        if(drink.strIngredient13 != null){
-            cocktailIngredients.innerHTML +=
-            `<span class='measurement'>${drink.strMeasure15 } of </span>
-        <span class='ingredient'>${drink.strIngredient15 }, </span>`
-        }  
         showDrink.innerHTML += `<h4 class='subject'>Glass </h4><p id='glassType'>${glass}</p>`
         showDrink.innerHTML += `<h4 class='subject'>Instructions </h4><p id='preparation'>${instructions}</p>`
     }catch(error){
         alert('That drink is not in our database')
         console.log('create cocktail not currently wokring')
     }
-
 }
-//look up a random drink name
-//see if you can return the drink AND ingredients
-
 document.querySelector('input').addEventListener('input', updateValue)
 document.querySelector('.cocktailbtn').addEventListener('click', getCocktail)
 document.querySelector('.randomCocktail').addEventListener('click', randomCocktail)    
-    
-console.log('input')
